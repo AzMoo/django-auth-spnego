95df68317f0335ff124798cf747fa121fcab4dae
Matt Magin <matt.magin@cmv.com.au>
2017-09-05 21:24:02 +0930

    Created structure ready for packaging and set the version appropriate for an alpha.

--- setup.py
+++ setup.py
@@ -2,11 +2,28 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-auth-spnego',
-    version='1.0.0',
+    version='0.1.0',
     author='Matt Magin',
     author_email='matt.azmoo@gmail.com',
-    description='SPNEGO-based Kerberos and NTLM authentication in django',
+    url='https://github.com/AzMoo/django-auth-spnego',
+    description='SPNEGO Kerberos authentication middleware for django',
     license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.10',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP',  # noqa
+    ],
+    keywords='django spnego auth ldap middleware',
     packages=find_packages(),
     install_requires=[
         'django',
