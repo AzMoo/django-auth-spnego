62b911e5524f067622f0f14fcfc13095d5249536
Matt Magin <matt.magin@cmv.com.au>
2017-09-05 22:05:07 +0930

    Bump minor version for test and add build to gitignore.

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import find_packages, setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
     name='django-auth-spnego',
-    version='0.1.0',
+    version='0.1.1',
     author='Matt Magin',
     author_email='matt.azmoo@gmail.com',
     url='https://github.com/AzMoo/django-auth-spnego',
     description='SPNEGO Kerberos authentication middleware for django',
+    long_description=readme(),
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -29,5 +36,6 @@ setup(
         'django',
         'kerberos',
         'django-auth-ldap'
-    ]
+    ],
+    python_requires='>=3.4'
 )
